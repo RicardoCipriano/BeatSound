e1e9caabf288b4b5eb734ff1b6fce0d3296f0f26
RicardoCipriano <61168955+RicardoCipriano@users.noreply.github.com>
2026-05-11 19:42:13 -0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DISCOGS_USER_TOKEN = "fwUaYzaXgLuNvNNtSDejTgrWIBknEPVuuJBTvUMZ"
-LASTFM_API_KEY = "935b28c00372c0b2a9eb3d8bf9d4f4bf"
-SPOTIFY_CLIENT_ID = "222263bf7a394b0f8b28027f03f0fba0"
-SPOTIFY_CLIENT_SECRET = "7c3fd0f45b384ea1ab82431315ebcf65"
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, 'music.db')
