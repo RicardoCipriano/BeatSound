77eef3f2c89fe43ab045ce5abae10b92caef8185
RicardoCipriano <61168955+RicardoCipriano@users.noreply.github.com>
2026-05-11 19:45:06 -0300

    Delete run_app.bat

--- run_app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "TCL_LIBRARY=C:\Users\Ricardo\AppData\Local\Programs\Python\Python313\tcl\tcl8.6"
-set "TK_LIBRARY=C:\Users\Ricardo\AppData\Local\Programs\Python\Python313\tcl\tk8.6"
-"C:\projetos\SearchMusicBeat\venv\Scripts\python.exe" main.py
-pause
